1d150528053ba24eb7e44c22ed88bc3a56789a35
Harold de Jong <hrjong@ziggo.nl>
2020-10-16 10:53:49 +0000

    3e serverready

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ge!dc*&@4#jf#lbx+4w$#*a2%h0hg_w_g4da@_edfs7_%jz$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Harolddejong.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 LOGIN_URL ='/login/'
 
